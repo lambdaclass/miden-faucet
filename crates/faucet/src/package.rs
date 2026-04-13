d5db7ec997d64c24adb142b9ae8ab466610e9ef6
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2026-04-13 18:24:03 -0300

    (WIP): update faucet

--- crates/faucet/src/package.rs
+++ crates/faucet/src/package.rs
@@ -1,7 +1,6 @@
 use std::path::Path;
 
 use anyhow::{Context, bail};
-use cargo_miden::{OutputType, run};
 use miden_client::Deserializable;
 use miden_client::vm::Package;
 
@@ -17,30 +16,29 @@ use miden_client::vm::Package;
 /// # Errors
 /// Returns an error if compilation fails or if the output is not in the expected format
 pub fn build_project_in_dir(dir: &Path, release: bool) -> anyhow::Result<Package> {
-    let profile = if release { "--release" } else { "--debug" };
-    let manifest_path = dir.join("Cargo.toml");
-    let manifest_arg = manifest_path.to_string_lossy();
+    // let profile = if release { "--release" } else { "--debug" };
+    // let manifest_path = dir.join("Cargo.toml");
+    // let manifest_arg = manifest_path.to_string_lossy();
 
-    let args = vec!["cargo", "miden", "build", profile, "--manifest-path", &manifest_arg];
+    // let args = vec!["cargo", "miden", "build", profile, "--manifest-path", &manifest_arg];
 
-    let output = run(args.into_iter().map(String::from), OutputType::Masm)
-        .context("Failed to compile project")?
-        .context("Cargo miden build returned None")?;
+    // let output = run(args.into_iter().map(String::from), OutputType::Masm)
+    //     .context("Failed to compile project")?
+    //     .context("Cargo miden build returned None")?;
 
-    let artifact_path = match output {
-        cargo_miden::CommandOutput::BuildCommandOutput { output } => match output {
-            cargo_miden::BuildOutput::Masm { artifact_path } => artifact_path,
-            other @ cargo_miden::BuildOutput::Wasm { .. } => {
-                bail!("Expected Masm output, got {other:?}")
-            },
-        },
-        other @ cargo_miden::CommandOutput::NewCommandOutput { .. } => {
-            bail!("Expected BuildCommandOutput, got {other:?}")
-        },
-    };
-
-    let package_bytes = std::fs::read(&artifact_path)
-        .context(format!("Failed to read compiled package from {}", artifact_path.display()))?;
+    // let artifact_path = match output {
+    //     cargo_miden::CommandOutput::BuildCommandOutput { output } => match output {
+    //         cargo_miden::BuildOutput::Masm { artifact_path } => artifact_path,
+    //         other @ cargo_miden::BuildOutput::Wasm { .. } => {
+    //             bail!("Expected Masm output, got {other:?}")
+    //         },
+    //     },
+    //     other @ cargo_miden::CommandOutput::NewCommandOutput { .. } => {
+    //         bail!("Expected BuildCommandOutput, got {other:?}")
+    //     },
+    // };
+    let package_bytes = std::fs::read("/Users/fabri/Repositories/miden-faucet/target/miden/debug/miden_faucet_mint_tx.masp")
+        .context(format!(""))?;
 
     Package::read_from_bytes(&package_bytes).context("Failed to deserialize package from bytes")
 }
